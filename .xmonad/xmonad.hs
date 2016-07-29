f106e0952e8570aa04bbf1d6d680e875ab7a9ad6
Mike Izbicki <mike@izbicki.me>
2016-07-29 11:22:26 -0700

    updated to debian 8

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,5 @@
 import XMonad
-import XMonad.Config.Gnome
+import XMonad.Config.Mate
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -7,13 +7,18 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
---     spawnPipe "/home/user/.dropbox-dist/dropboxd"
-    xmonad $ gnomeConfig
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/user/.xmobarrc"
+    xmonad $ defaultConfig
         { terminal = "uxterm"
         , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-	} `additionalKeys`
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+	    }
+        `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
