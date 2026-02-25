3f625225444c2f144f077e87cc5494de5a2ff948
Mike Izbicki <mike@izbicki.me>
2026-02-25 09:22:19 -0800

    fix vpn script

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-/sbin/openconnect --user=mizbicki@cmc.edu --protocol=anyconnect https://vpn.claremontmckenna.edu/ --os=win --useragent='AnyConnect Windows 4.9.00086'
+/sbin/openconnect --user=mizbicki@cmc.edu --protocol=anyconnect https://vpn.claremontmckenna.edu/ --os=win --useragent='AnyConnect Windows 4.9.00086' --no-external-auth
+
+# FIXME:
+# this needs to be manually typed into a different terminal to fix DNS issues with the VPN
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
